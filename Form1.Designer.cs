e7eea1f98591e446013a59d93fe95bdf904ca94a
Adın Soyadın <mmusayeva@std.beu.edu.az>
2025-10-01 00:09:13 +0400

    Layihənin bütün faylları əlavə edildi

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace calculate2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            AC = new Button();
+            button2 = new Button();
+            qaliq = new Button();
+            devide = new Button();
+            vur = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            topla = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            cix = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            beraber = new Button();
+            button18 = new Button();
+            btn0 = new Button();
+            button20 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(-1, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(502, 184);
+            listBox1.TabIndex = 0;
+            // 
+            // AC
+            // 
+            AC.Location = new Point(29, 205);
+            AC.Name = "AC";
+            AC.Size = new Size(70, 58);
+            AC.TabIndex = 1;
+            AC.Text = "AC";
+            AC.UseVisualStyleBackColor = true;
+            AC.Click += AC_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(144, 208);
+            button2.Name = "button2";
+            button2.Size = new Size(78, 57);
+            button2.TabIndex = 2;
+            button2.Text = "+/-";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // qaliq
+            // 
+            qaliq.Location = new Point(273, 207);
+            qaliq.Name = "qaliq";
+            qaliq.Size = new Size(74, 58);
+            qaliq.TabIndex = 3;
+            qaliq.Text = "%";
+            qaliq.UseVisualStyleBackColor = true;
+            qaliq.Click += qaliq_Click;
+            // 
+            // devide
+            // 
+            devide.Location = new Point(391, 207);
+            devide.Name = "devide";
+            devide.Size = new Size(78, 56);
+            devide.TabIndex = 4;
+            devide.Text = "/";
+            devide.UseVisualStyleBackColor = true;
+            devide.Click += devide_Click;
+            // 
+            // vur
+            // 
+            vur.Location = new Point(391, 288);
+            vur.Name = "vur";
+            vur.Size = new Size(78, 56);
+            vur.TabIndex = 8;
+            vur.Text = "x";
+            vur.UseVisualStyleBackColor = true;
+            vur.Click += vur_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(273, 288);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(74, 58);
+            btn3.TabIndex = 7;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(144, 289);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(78, 57);
+            btn2.TabIndex = 6;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(29, 286);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(70, 58);
+            btn1.TabIndex = 5;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // topla
+            // 
+            topla.Location = new Point(391, 373);
+            topla.Name = "topla";
+            topla.Size = new Size(78, 56);
+            topla.TabIndex = 12;
+            topla.Text = "+";
+            topla.UseVisualStyleBackColor = true;
+            topla.Click += topla_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(273, 373);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(74, 58);
+            btn6.TabIndex = 11;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(144, 374);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(78, 57);
+            btn5.TabIndex = 10;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(29, 371);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(70, 58);
+            btn4.TabIndex = 9;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // cix
+            // 
+            cix.Location = new Point(391, 456);
+            cix.Name = "cix";
+            cix.Size = new Size(78, 56);
+            cix.TabIndex = 16;
+            cix.Text = "-";
+            cix.UseVisualStyleBackColor = true;
+            cix.Click += cix_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(273, 456);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(74, 58);
+            btn9.TabIndex = 15;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(144, 457);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(78, 57);
+            btn8.TabIndex = 14;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(29, 454);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(70, 58);
+            btn7.TabIndex = 13;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // beraber
+            // 
+            beraber.Location = new Point(391, 544);
+            beraber.Name = "beraber";
+            beraber.Size = new Size(78, 56);
+            beraber.TabIndex = 20;
+            beraber.Text = "=";
+            beraber.UseVisualStyleBackColor = true;
+            beraber.Click += beraber_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(273, 544);
+            button18.Name = "button18";
+            button18.Size = new Size(74, 58);
+            button18.TabIndex = 19;
+            button18.Text = ",";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(144, 545);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(78, 57);
+            btn0.TabIndex = 18;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(29, 542);
+            button20.Name = "button20";
+            button20.Size = new Size(70, 58);
+            button20.TabIndex = 17;
+            button20.Text = "sin";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(502, 654);
+            Controls.Add(beraber);
+            Controls.Add(button18);
+            Controls.Add(btn0);
+            Controls.Add(button20);
+            Controls.Add(cix);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(topla);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(vur);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(devide);
+            Controls.Add(qaliq);
+            Controls.Add(button2);
+            Controls.Add(AC);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button AC;
+        private Button button2;
+        private Button qaliq;
+        private Button devide;
+        private Button vur;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button topla;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button cix;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button beraber;
+        private Button button18;
+        private Button btn0;
+        private Button button20;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+
+namespace calculate2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e) => listBox1.Items.Add("1");
+        private void btn2_Click(object sender, EventArgs e) => listBox1.Items.Add("2");
+        private void btn3_Click(object sender, EventArgs e) => listBox1.Items.Add("3");
+        private void btn4_Click(object sender, EventArgs e) => listBox1.Items.Add("4");
+        private void btn5_Click(object sender, EventArgs e) => listBox1.Items.Add("5");
+        private void btn6_Click(object sender, EventArgs e) => listBox1.Items.Add("6");
+        private void btn7_Click(object sender, EventArgs e) => listBox1.Items.Add("7");
+        private void btn8_Click(object sender, EventArgs e) => listBox1.Items.Add("8");
+        private void btn9_Click(object sender, EventArgs e) => listBox1.Items.Add("9");
+        private void btn0_Click(object sender, EventArgs e) => listBox1.Items.Add("0");
+
+        private void topla_Click(object sender, EventArgs e) => listBox1.Items.Add("+");
+        private void cix_Click(object sender, EventArgs e) => listBox1.Items.Add("-");
+        private void vur_Click(object sender, EventArgs e) => listBox1.Items.Add("*");
+        private void devide_Click(object sender, EventArgs e) => listBox1.Items.Add("/");
+        private void qaliq_Click(object sender, EventArgs e) => listBox1.Items.Add("%");
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void beraber_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string expression = string.Join("", listBox1.Items.Cast<string>());
+
+                
+                DataTable dt = new DataTable();
+                var result = dt.Compute(expression, "");
+
+         
+                listBox1.Items.Clear();
+                listBox1.Items.Add(result?.ToString() ?? "sehv ifade");
+            }
+            catch
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Error");
+            }
+        }
+    }
+}
